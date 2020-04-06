fec3e36fccaac07ae7c62ef1dcca5f12fa5f41e3
pedroromn <pedroromn@gmail.com>
2020-04-05 19:01:43 -0500

    Add project files.

---
+++ first_project/first_project.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, first programm!" << std::endl;
+
+	return 0;
+}
---
+++ predefined_functions/predefined_functions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <cctype>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	//TODO
+	// page 351 - D.D Malik 8th Ed
+	return 0;
+}
